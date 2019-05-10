fc37afee27bb31bb3e323290d0818e46cf32790f
Ally Netzel <netzelally@gmail.com>
2019-05-10 15:20:20 +0000

    Automatically backed up by Learn

--- src/Comment.js
+++ src/Comment.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react'
 
 class Comment extends Component {
-  
+  render(){
+    return (
+      <div className="comment"/>
+      )
+  }
 }
 
 export default Comment
